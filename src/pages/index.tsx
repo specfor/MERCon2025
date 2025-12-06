a877b516b8d0461a767110330acbb0ea95352cc2
Vihanga Nimsara <vihanga2003nimsara@gmail.com>
2025-12-06 22:29:05 +0530

    feat: Add ResearchFields component to display conference tracks and streamline index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
-import { Link as GatsbyLink } from "gatsby";
 import Gallery from "../components/homepage/gallery";
 import OrganizerSection from "../components/homepage/organizer";
 import SectionHeader from "../components/sectionHeader";
 import UniGallery from "../components/homepage/uniGallery";
+import ResearchFields from "../components/homepage/fields";
 
 // Hero background image
 const heroBackground = (
@@ -17,61 +17,6 @@ const heroBackground = (
     />
   </div>
 );
-
-// Static imports for track images
-const trackImages = {
-  track1: (
-    <StaticImage
-      src="../images/tracks/1y.png"
-      alt="Data Science and Artificial Intelligence"
-      className="w-full h-full"
-    />
-  ),
-  track2: <StaticImage src="../images/tracks/2y.png" alt="Biomedical Engineering" className="w-full h-full" />,
-  track3: <StaticImage src="../images/tracks/3y.png" alt="Chemical Engineering" className="w-full h-full" />,
-  track4: <StaticImage src="../images/tracks/4y.png" alt="Civil Engineering" className="w-full h-full" />,
-  track5: <StaticImage src="../images/tracks/5y.png" alt="Environmental Engineering" className="w-full h-auto" />,
-  track6: <StaticImage src="../images/tracks/6y.png" alt="Electronic Engineering" className="w-full h-full" />,
-  track7: (
-    <StaticImage src="../images/tracks/7y.png" alt="Engineering Mathematics & Statistics" className="w-full h-full" />
-  ),
-  track8: <StaticImage src="../images/tracks/8y.png" alt="Signal Processing" className="w-full h-full" />,
-  track9: (
-    <StaticImage src="../images/tracks/9y.png" alt="Materials Science and Engineering" className="w-full h-full" />
-  ),
-  track10: (
-    <StaticImage src="../images/tracks/10y.png" alt="Mechanical Engineering Systems" className="w-full h-full" />
-  ),
-  track11: (
-    <StaticImage src="../images/tracks/11y.png" alt="Micro/Nano Electro Mechanical Systems" className="w-full h-full" />
-  ),
-  track12: (
-    <StaticImage src="../images/tracks/12y.png" alt="Mining and Mineral Exploration" className="w-full h-full" />
-  ),
-  track13: (
-    <StaticImage
-      src="../images/tracks/13y.png"
-      alt="Electrical Machines & Solid State Drives"
-      className="w-full h-full"
-    />
-  ),
-  track14: (
-    <StaticImage src="../images/tracks/14y.png" alt="Robotics and Intelligent Systems" className="w-full h-full" />
-  ),
-  track15: (
-    <StaticImage
-      src="../images/tracks/15y.png"
-      alt="Software Engineering and Cloud Computing"
-      className="w-full h-full"
-    />
-  ),
-  track16: <StaticImage src="../images/tracks/16y.png" alt="Sustainable Energy" className="w-full h-full" />,
-  track17: <StaticImage src="../images/tracks/17y.png" alt="Textile and Apparel" className="w-full h-full" />,
-  track18: (
-    <StaticImage src="../images/tracks/18y.png" alt="Engineering Technology Management" className="w-full h-full" />
-  ),
-  track19: <StaticImage src="../images/tracks/19y.png" alt="Transportation Engineering and Logistics" />,
-};
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
@@ -126,52 +71,7 @@ const IndexPage: React.FC<PageProps> = () => {
       <div className="grow w-full bg-black">
         {/* Call for Papers Section */}
         <section id="call-for-papers" className="w-full py-20 px-6 md:px-12">
-          <div className="max-w-5xl mx-auto">
-            <h3 className="text-lg text-center text-gray-200 mb-12">
-              MERCon 2025 solicits research papers describing significant & innovative research contributions to
-              following areas of engineering. We will invite submissions on a wide range of research topics.
-            </h3>
-
-            <h3 className="text-2xl font-bold text-center text-white mb-12">
-              Papers can be submitted to the conference under the following regular tracks.
-            </h3>
-
-            {/* Tracks Grid */}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10">
-              {[
-                { title: "Data Science and Artificial Intelligence", key: "track1" },
-                { title: "Biomedical Engineering", key: "track2" },
-                { title: "Chemical Engineering", key: "track3" },
-                { title: "Civil Engineering", key: "track4" },
-                { title: "Environmental Engineering", key: "track5" },
-                { title: "Electronic Engineering", key: "track6" },
-                { title: "Engineering Mathematics & Statistics", key: "track7" },
-                { title: "Signal Processing", key: "track8" },
-                { title: "Materials Science and Engineering", key: "track9" },
-                { title: "Mechanical Engineering Systems", key: "track10" },
-                { title: "Micro/Nano Electro Mechanical Systems, Mechatronics, and Micromechatronics", key: "track11" },
-                { title: "Mining and Mineral Exploration", key: "track12" },
-                { title: "Electrical Machines & Solid State Drives", key: "track13" },
-                { title: "Robotics and Intelligent Systems", key: "track14" },
-                { title: "Software Engineering and Cloud Computing", key: "track15" },
-                { title: "Sustainable Energy", key: "track16" },
-                { title: "Textile and Apparel", key: "track17" },
-                { title: "Engineering Technology Management", key: "track18" },
-                { title: "Transportation Engineering and Logistics", key: "track19" },
-              ].map((track, index) => (
-                <GatsbyLink key={index} to="/call-for-papers" className="block">
-                  <div className="bg-dark-800 border-2 w-full h-fit border-primary-700 rounded-3xl hover:border-primary-500 transition-colors">
-                    <div className="w-full h-auto p-10 aspect-square rounded-4xl bg-dark-900">
-                      {trackImages[track.key as keyof typeof trackImages]}
-                    </div>
-                  </div>
-                  <p className="p-4 text-center text-gray-200 text-sm font-medium group-hover:text-primary-500 transition-colors grow flex items-start justify-center">
-                    {track.title}
-                  </p>
-                </GatsbyLink>
-              ))}
-            </div>
-          </div>
+          <ResearchFields />
         </section>
 
         {/* Gallery Section */}
